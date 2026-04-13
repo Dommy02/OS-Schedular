1fe5afc774c40dbd91bd103ce5131661c6a6e97a
M-Medhat25 <m.medhat25.04@gmail.com>
2026-04-13 10:49:40 +0200

    Fixed the generator and used FCFS(1 cpu) for scheduler

--- process_generator.c
+++ process_generator.c
@@ -1,11 +1,13 @@
 #include "headers.h"
+
 typedef struct process{
     long mtype;
     int id,arrival,runTime,priority;
+    char state;
+    int remainigTime,WaitingTime,whichCpu;
 } process;
 
 void clearResources(int);
-
 
 int countLines(const char *fileName)
 {
@@ -28,70 +30,126 @@ int countLines(const char *fileName)
 int main(int argc, char * argv[])
 {
     signal(SIGINT, clearResources);
-    // TODO Initialization
-    // 1. Read the input files.
-    int processesNum = countLines("processes.txt") - 1;// -1 duo to the first line
-    process* arr; 
-    arr = (process*) malloc(processesNum * sizeof(process));
+    // read input file
+    int processesNum = countLines("processes.txt") - 1; // -1 due to the first line
+    if (processesNum <= 0)
+    {
+        fprintf(stderr, "No valid process rows found in processes.txt\n");
+        exit(1);
+    }
+
+    process* arr = (process*) malloc(processesNum * sizeof(process));
+    if (!arr)
+    {
+        perror("malloc failed");
+        exit(1);
+    }
+    
     FILE *fp = fopen("processes.txt", "r");
+    if (!fp)
+    {
+        perror("fopen failed");
+        free(arr);
+        exit(1);
+    }
+
     char buffer[100];
-    fgets(buffer, sizeof(buffer), fp);
+    fgets(buffer, sizeof(buffer), fp); // skip header
+
     int counter = 0;
-    while(counter != processesNum){
-        fscanf(fp,"%d %d %d %d",&arr[counter].id,&arr[counter].arrival,&arr[counter].runTime,&arr[counter].priority);
+    while (counter < processesNum && fscanf(fp, "%d %d %d %d", &arr[counter].id, &arr[counter].arrival, &arr[counter].runTime, &arr[counter].priority) == 4)
+    {
+        arr[counter].mtype = 1;
+        arr[counter].state = 'N';
+        arr[counter].remainigTime = arr[counter].runTime;
+        arr[counter].WaitingTime = 0;
+        arr[counter].whichCpu = 0;
         counter++;
     }
-    // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-    printf("Choose an algorithm: 1)HPF  2)RR   3)2-cpu FCFS: ");
-    int algNum,rrSlot = 0;
-    scanf("%d",&algNum);
-    if(algNum == 2){
+    fclose(fp);
 
+    // Ask the user for the chosen scheduling algorithm and its parameters if exist
+    int algNum = 0, rrSlot = 0, N = 0, M = 0;
+    printf("Choose an algorithm: 1)HPF  2)RR   3)2-cpu FCFS: ");
+    scanf("%d", &algNum);
+    if (algNum == 2)
+    {
         printf("Enter your time slot: ");
-        scanf("%d",&rrSlot);
+        scanf("%d", &rrSlot);
     }
-    // 3. Initiate and create the scheduler and clock processes.
+    if(algNum == 3){
+        printf("Enter your time slot \"N\" and ThresHold \"M\" : ");
+        scanf("%d %d", &N, &M );
+    }
+
+    // Start clock
     int clkPid = fork();
-    if(clkPid == 0){
+    if (clkPid == 0)
+    {
         char *clk_argv[] = {"./clk.out", NULL};
-        execv("./clk.out",clk_argv);
+        execv("./clk.out", clk_argv);
+        perror("execv clk.out failed");
+        exit(1);
     }
-    int schedularPid = fork();
-    if(schedularPid == 0){
-    
-        char algStr[10], rrStr[10];
+    // start the message queue between generator and scheduler
+    int process_index = 0;
+    key_t msgQueue_key = ftok("clk.c", 10);
+    int msgQueue_id = msgget(msgQueue_key, 0666 | IPC_CREAT);
+    if (msgQueue_id == -1)
+    {
+        perror("msgget failed");
+        free(arr);
+        destroyClk(true);
+        exit(1);
+    }
+
+    msgQueue_id = msgget(msgQueue_key, 0666 | IPC_CREAT);
+    if (msgQueue_id == -1)
+    {
+        perror("msgget recreate failed");
+        free(arr);
+        destroyClk(true);
+        exit(1);
+    }
+
+    // start scheduler
+    int schedulerPid = fork();
+    if (schedulerPid == 0)
+    {
+        char processNumStr[16], algStr[16], rrStr[16], Nstr[16],Mstr[16];
+        sprintf(processNumStr, "%d", processesNum);
         sprintf(algStr, "%d", algNum);
         sprintf(rrStr, "%d", rrSlot);
-        char *sch_argv[] = {"./scheduler.out",algStr,rrStr, NULL};
-        execv("./scheduler.out",sch_argv);
-    }
-    // 4. Use this function after creating the clock process to initialize clock
-    initClk();
-    // To get time use this
-    int x = getClk();
-    printf("current time is %d\n", x);
-    // TODO Generation Main Loop
-    // 5. Create a data structure for processes and provide it with its parameters. & 6. Send the information to the scheduler at the appropriate time.
-    int process_index = 0;
-    key_t msgQueue_key;
-    int msgQueue_id;
-    msgQueue_key = ftok("clk.c", 10);
-    msgQueue_id = msgget(msgQueue_key, 0666 | IPC_CREAT);
+        sprintf(Nstr, "%d", N);
+        sprintf(Mstr, "%d", M);
 
-    while(process_index != processesNum){
+        char *sch_argv[] = {"./scheduler.out", processNumStr, algStr, rrStr,Nstr,Mstr, NULL};
+        execv("./scheduler.out", sch_argv);
+        perror("execv scheduler.out failed");
+        exit(1);
+    }
+    // start to pass the processes when it's arrival time come
+    initClk();
+
+    while (process_index < processesNum)
+    {
         int now = getClk();
-        while(arr[process_index].arrival == now){
-            // send in message queue
-            arr[process_index].mtype = 1;
-            msgsnd(msgQueue_id, &arr[process_index],sizeof(process) - sizeof(long), !IPC_NOWAIT);
-            process_index++;
+        while (process_index < processesNum && arr[process_index].arrival <= now)
+        {
+            if (msgsnd(msgQueue_id, &arr[process_index], sizeof(process) - sizeof(long), 0) != -1)
+            {
+                process_index++;
+            }
         }
     }
-    // 7. Clear clock resources
+
+    free(arr);
+    waitpid(schedulerPid, NULL, 0);
     destroyClk(true);
+    return 0;
 }
 
 void clearResources(int signum)
 {
-    //TODO Clears all resources in case of interruption
+    destroyClk(true);
 }
--- scheduler.c
+++ scheduler.c
@@ -1,12 +1,105 @@
 #include "headers.h"
 
+typedef struct process{
+    long mtype;
+    int id,arrival,runTime,priority;
+    char state;
+    int remainigTime,WaitingTime,whichCpu;
+} process;
 
 int main(int argc, char * argv[])
 {
+    int totalProcesses = atoi(argv[1]);
+
     initClk();
+    printf("Scheduler started at t=%d\n", getClk());
+
+    process *PCB = (process *)malloc(sizeof(process) * (1+totalProcesses));// 1 indexed
+    process *readyQueue = (process *)malloc(sizeof(process) * (1+totalProcesses));
+
+    int head = 0, tail = 0;
+    int receivedCount = 0, finishedCount = 0;
+
+    key_t msgQueue_key = ftok("clk.c", 10);
+    int msgQueue_key_id = msgget(msgQueue_key, 0666 | IPC_CREAT);
     
-    //TODO implement the scheduler :)
-    //upon termination release the clock resources.
+    bool cpuBusy = false;
+    pid_t runningPid = -1;
+    int runningId = -1;
+
+    while (finishedCount < totalProcesses)
+    {
+        // Receive all currently available processes from bus
+        while (receivedCount < totalProcesses)
+        {
+            process messageBuff;
+            msgrcv(msgQueue_key_id, &messageBuff, sizeof(process) - sizeof(long), 1, IPC_NOWAIT);
+
+            int id = messageBuff.id;
+            PCB[id] = messageBuff;
+
+            // Insert in Ready Queue
+            if (tail <= totalProcesses)
+            {
+                readyQueue[tail++] = messageBuff;
+                printf("Enqueued P%d at t=%d (runtime=%d)\n", id, getClk(), messageBuff.runTime);
+            }
+            receivedCount++;
+        }
+
+        // Dispatch FCFS if CPU idle
+        if (!cpuBusy && head < tail)
+        {
+            process p = readyQueue[head++];
+            runningId = p.id;
+            pid_t pid = fork();
+            if (pid == -1)
+            {
+                perror("fork failed");
+                free(PCB);
+                
+                free(readyQueue);
+                destroyClk(false);
+                return 1;
+            }
+
+            if (pid == 0)
+            {
+                
+                for (int rem = p.runTime; rem > 0; rem--)
+                {
+                    printf("P%d remaining=%d\n", p.id, rem);
+                    sleep(1);
+                }
+                printf("P%d remaining=0\n", p.id);
+                _exit(0);
+            }
+
+            runningPid = pid;
+            cpuBusy = true;
+            printf("Started P%d at t=%d\n", runningId, getClk());
+        }
+
+        // Check running child
+        if (cpuBusy)
+        {
+            int status;
+            pid_t w = waitpid(runningPid, &status, WNOHANG);
+            if (w == runningPid)
+            {
+                
+                finishedCount++;
+                printf("Finished P%d at t=%d\n", runningId, getClk());
+                cpuBusy = false;
+                runningPid = -1;
+                runningId = -1;
+            }
+        }
+    }
+
+    free(PCB);
     
-    destroyClk(true);
+    free(readyQueue);
+    destroyClk(false);
+    return 0;
 }
